b82d8359bf89c051191d1fdaa5e0ba9d99bae5fc
ahmadzamroni015 <ahmadzamroni015@gmail.com>
2026-05-15 21:28:18 +0700

    Update all files for production

--- sosmed/admin.py
+++ sosmed/admin.py
@@ -1,16 +1,4 @@
-# sosmed/admin.py
 from django.contrib import admin
-from django.contrib.admin import AdminSite
 from .models import Instagram
 
-class CustomAdminSite(AdminSite):
-    site_header = 'ELECTRIC XTRA - Admin Panel'
-    site_title = 'Social Media Manager'
-    index_title = 'Dashboard'
-
-# Register model
-@admin.register(Instagram)
-class InstagramAdmin(admin.ModelAdmin):
-    list_display = ['nama_depan', 'nama_belakang', 'username']
-    search_fields = ['nama_depan', 'nama_belakang', 'username']
-    list_per_page = 20
+admin.site.register(Instagram)
